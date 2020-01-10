182695dbd2932252340cc87d0d931325b359bc43
MinG822 <lucida822@naver.com>
2020-01-10 16:51:03 +0900

    set vuetify and vue2-google-maps

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
+import VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'YOUR_API_KEY',
+    v: 'GOOGLE_MAPS_VERSION',
+    libraries: 'places'
+  }
+})
+
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
